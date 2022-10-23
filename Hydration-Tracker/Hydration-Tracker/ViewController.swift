d8b9c376615bdfe75804fa80b0333f55077133fc
Abdu512 <abdullahbaghdadi@utexas.edu>
2022-10-23 12:27:51 -0500

    Create project and pods

---
+++ Hydration-Tracker/Hydration-Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hydration-Tracker
+//
+//  Created by Abdullah Baghdadi on 10/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hydration-Tracker/Pods/Target Support Files/Pods-Hydration-Tracker-Hydration-TrackerUITests/Pods-Hydration-Tracker-Hydration-TrackerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hydration_Tracker_Hydration_TrackerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Hydration_Tracker_Hydration_TrackerUITests
+@end
