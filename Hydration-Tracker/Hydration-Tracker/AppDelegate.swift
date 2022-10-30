e510a0c86d737818ca105cedbc7e96f532a0d91f
Abdu512 <abdullahbaghdadi@utexas.edu>
2022-10-30 12:50:51 -0500

    Sign Up

--- Hydration-Tracker/Hydration-Tracker/AppDelegate.swift
+++ Hydration-Tracker/Hydration-Tracker/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "SSbJIG2GLwYxWlS03JcdvvrFC50f2tECnwyZGMXZ"
+                    $0.clientKey = "8nkaWLQWgvN4p4sQ1psgmjl7eKTIWZeJ2mFGgJVb"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
