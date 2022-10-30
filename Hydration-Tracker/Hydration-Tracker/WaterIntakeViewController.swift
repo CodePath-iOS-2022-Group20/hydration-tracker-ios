e591d45a15798617d6ef31e5540431349e1f2bb0
Abdu512 <abdullahbaghdadi@utexas.edu>
2022-10-30 15:00:31 -0500

    Water Adding Screen

--- Hydration-Tracker/Hydration-Tracker/WaterIntakeViewController.swift
+++ Hydration-Tracker/Hydration-Tracker/WaterIntakeViewController.swift
@@ -14,24 +14,35 @@ class WaterIntakeViewController: UIViewController {
     
     @IBOutlet weak var waterAmountField: UITextField!
     @IBOutlet weak var addOrLowerSelection: UISegmentedControl!
-    
+    @IBOutlet weak var waterAmountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let water = user?["daily_water"]
+        waterAmountLabel.text = ("\(water ?? "nil")") + "mL"
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func onSubmit(_ sender: Any) {
-        var current_water = user?["daily_water"] as! Int
-        var new_water = current_water + 50
-        user?.setValue(new_water, forKey: "daily_water")
         
-//        switch addOrLowerSelection.selectedSegmentIndex {
-//        case 0:
-//            user["daily_water"] +=
-//        case 1:
-//        }
+        if addOrLowerSelection.selectedSegmentIndex == 0 {
+            let waterAmountText = waterAmountField.text ?? ""
+            let waterAmount = Int(waterAmountText) ?? 0
+            user?["daily_water"] = user?["daily_water"] as! Int + waterAmount
+            user?.saveInBackground()
+            let water = user?["daily_water"]
+            waterAmountLabel.text = ("\(water ?? "nil")") + "mL"
+        }
+        
+        if addOrLowerSelection.selectedSegmentIndex == 1 {
+            let waterAmountText = waterAmountField.text ?? ""
+            let waterAmount = Int(waterAmountText) ?? 0
+            user?["daily_water"] = user?["daily_water"] as! Int - waterAmount
+            user?.saveInBackground()
+            let water = user?["daily_water"]
+            waterAmountLabel.text = ("\(water ?? "nil")") + "mL"
+        }
     }
     
     /*
