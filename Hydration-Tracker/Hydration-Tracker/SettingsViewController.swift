3e842aa2b3748819c450b19e3ab8baabcda9e100
coolMohi <coolmohhi@gmail.com>
2022-11-01 20:52:41 -0500

    settings commit 1

--- Hydration-Tracker/Hydration-Tracker/SettingsViewController.swift
+++ Hydration-Tracker/Hydration-Tracker/SettingsViewController.swift
@@ -6,19 +6,33 @@
 //
 
 import UIKit
+import Parse
 
 class SettingsViewController: UIViewController {
-
+    @IBOutlet weak var changeGoal: UILabel!
+    @IBOutlet weak var changeGoalField: UITextField!
+    @IBOutlet var notifications: UIView!
+    @IBOutlet weak var notiSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        let user = PFUser()
+        user["water_goal"] = changeGoalField.text
+        print(user["water_goal"])
+    }
+    
+    
+    
     
 
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
